554b92dec94564b1d8e4ebcdbc91505c61117cc8
Wyatt Johnston <wyjohnston@gmail.com>
2014-05-23 16:08:49 -0400

    Add NavCtrl, move submit post to NavCtrl, add Nav bar

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -7,24 +7,6 @@ app.controller('PostsCtrl', function ($scope, $location, Post){
 
   /* old implementation
 
-  $scope.submitPost = function() {
-    Post.save($scope.post, function (ref) {
-      $scope.posts[ref.name] = $scope.post;
-      $scope.post = {url: 'http://', title: ''};
-    });
-  };
-
-  */
-
-  $scope.submitPost = function() {
-    Post.create($scope.post).then(function(ref){
-      // $scope.post = {url: 'http://', title: ''};
-      $location.path('/posts/' + ref.name());
-    });
-  };
-
-  /* old implementation
-
   $scope.deletePost = function(postId) {
     Post.delete({id: postId}, function() {
       delete $scope.posts[postId];
