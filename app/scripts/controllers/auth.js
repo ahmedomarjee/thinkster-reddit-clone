11636c81e8148c5cdd0a01087e0229f9d2f0b7b4
Wyatt Johnston <wyjohnston@gmail.com>
2014-05-24 16:26:21 -0400

    Add user authentication, login and register pages, handle login/register errors

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -5,22 +5,25 @@ app.controller('AuthCtrl',
     if (Auth.signedIn()) {
       $location.path('/');
     }
-
+ 
     $scope.$on('$firebaseSimpleLogin:login', function () {
       $location.path('/');
     });
-
+ 
     $scope.login = function () {
       Auth.login($scope.user).then(function () {
-        $location.path('/')
-      })
+        $location.path('/');
+      }, function (error) {
+        $scope.error = error.toString();
+      });
     };
  
     $scope.register = function () {
       Auth.register($scope.user).then(function (authUser) {
-        console.log(authUser);
-        login();
+        Auth.login($scope.user);
         $location.path('/');
+      }, function (error) {
+        $scope.error = error.toString();
       });
     };
   });
