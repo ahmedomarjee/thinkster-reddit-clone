aa81251708a735170ce9beed5496e4b3944d40f6
Wyatt Johnston <wyjohnston@gmail.com>
2014-05-25 19:09:15 -0400

    Add voting, user profile pages

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -2,7 +2,9 @@
  
 app.controller('PostsCtrl', function ($scope, $location, Post){
   // $scope.posts = Post.get(); // retrieves posts from the DB with the Post $resource service
-  $scope.posts = Post.all;
+  if ($location.path() === '/') {
+    $scope.posts = Post.all;
+  }
   $scope.post = {url: 'http://', title: ''};
 
   /* old implementation
@@ -17,5 +19,29 @@ app.controller('PostsCtrl', function ($scope, $location, Post){
 
   $scope.deletePost = function(postId) {
     Post.delete(postId);
+  };
+
+  $scope.upVotePost = function (postId, upVoted) {
+    if (upVoted) {
+      Post.clearVote(postId, upVoted);
+    } else {
+      Post.upVote(postId);
+    }
+  };
+   
+  $scope.downVotePost = function (postId, downVoted) {
+    if (downVoted) {
+      Post.clearVote(postId, !downVoted);
+    } else {
+      Post.downVote(postId);
+    }
+  };
+   
+  $scope.upVoted = function (post) {
+    return Post.upVoted(post);
+  };
+   
+  $scope.downVoted = function (post) {
+    return Post.downVoted(post);
   };
 });
